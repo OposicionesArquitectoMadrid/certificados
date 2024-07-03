b3721e46830d7f866d0ee090f52613a053d5d215
Oposiciones arquitectos <gmotto.oposicionesarquitectos@gmail.com>
2024-07-03 19:14:44 +0200

    update

--- generarPdf.js
+++ generarPdf.js
@@ -17,17 +17,27 @@ function customDelay(ms) {
 
 
   async function generarPDF(students, selectedDirectory, dateInput, userInput, certType, pdfContent) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user:'academia.oposicionesarquitectos@gmail.com',
-            pass: process.env.PASSWORD,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-      });
+    // const transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user:'academia.oposicionesarquitectos@gmail.com',
+    //         pass: process.env.PASSWORD,
+    //     },
+    //     tls: {
+    //         rejectUnauthorized: false,
+    //     },
+    //   });
    
+    //   const transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 587,
+    //     secure: false, // true para usar TLS, false para no usarlo
+    //     auth: {
+    //         user:'academia.oposicionesarquitectos@gmail.com',
+    //         pass: process.env.PASSWORD,
+    //     },
+    // });
+        
 
     const { es } = require('date-fns/locale');
 
@@ -38,9 +48,11 @@ function customDelay(ms) {
 
     // students.forEach(async (student, index) => {
         for (const [index, student] of students.entries()) {
-          const doc = new PDFDocument({ size: 'letter' });
-   
-    const outputFileName = `${student.ALUMNO}_certificado.pdf`
+          const doc = new PDFDocument({ size: 'A4' });
+      
+
+
+    const outputFileName = `${student.ALUMNO}_CERTIFICADO SP A AYTO 2024.pdf`
     const outputPath = path.join(selectedDirectory, outputFileName) // se guarda donde selecciona el usuario 
     
 
@@ -53,7 +65,8 @@ function customDelay(ms) {
    // todo lo q es contenido del pdf
    //pagina membretada (fondo) común para todos
 
-   doc.image('./Administradora.jpg', 0, 0, { width: 612, height: 792 });
+   doc.image('./admin5.jpg', 0, 0, { width: 595, height: 842 }); // Tamaño A4: 595 x 842 puntos
+
 
    if (certType === 'inscripción') {
     // Certificado de inscripción
@@ -219,48 +232,63 @@ Y para que conste, firma en Madrid a ${formattedDate}`, { width: 400, align: 'le
 doc.end();
 }
 
-// Enviar correo 
-  const invoice = `${student.ALUMNO}_certificado.pdf`;
-    const recipient = student.EMAIL;
-    const subject = 'Certificado';
-    const body = `Estimado/a ${student.ALUMNO}, adjunto encontrarás tu certificado. Un cordial saludo`
+// // Enviar correo 
+//   const invoice = `${student.ALUMNO}_certificado.pdf`;
+//     const recipient = student.EMAIL;
+//     const subject = 'Certificado';
+//     const body = `Estimado/a ${student.ALUMNO}, adjunto encontrarás tu certificado. Un cordial saludo.
     
-      ;
+    
+//     * Por favor, no responda a este mensaje, ha sido enviado de forma automática. Si desea ponerse en contacto con nosotros para comentarnos alguna incidencia o mejora de este servicio, por favor, escríbanos a info@oposicionesarquitectos.com
 
-      const mailOptions = {
-        from: 'Oposiciones Arquitectos <academia.oposicionesarquitectos@gmail.com>',
-        to: recipient,
-        subject: subject,
-        text: body,
-        attachments: [
-          {
-            filename: invoice,
-            path: path.join(selectedDirectory, invoice),
-          },
-        ],
-      };
+// Oposiciones Arquitectos
+// C/. Orense 10 2º Oficina 10, 28020 - Madrid
+// www.oposicionesarquitectos.com
+
+
+
+// ---- ADVERTENCIA ---- La información contenida en este correo electrónico, y en su caso, cualquier fichero anexo al mismo, son de carácter privado y confidencial siendo para uso exclusivo de su destinatario. Si usted no es el destinatario correcto, el empleado o agente responsable de entregar el mensaje al destinatario, o ha recibido esta comunicación por error, le informamos que está totalmente prohibida cualquier divulgación, distribución o reproducción de esta comunicación según la legislación vigente y le rogamos que nos lo notifique inmediatamente, procediendo a su destrucción sin continuar su lectura.
+
+// Le informamos que, en cumplimiento del Reglamento (UE) 2016/679, su dirección de correo electrónico, así como el resto de los datos de carácter personal de la tarjeta de visita que nos facilite, podrían ser objeto de tratamiento automatizado en nuestros sistemas de información, con la finalidad de gestionar la agenda de contactos de OPOSICIONES ARQUITECTOS, S.L. Usted podrá en cualquier momento ejercer sus derechos de acceso, rectificación, cancelación, oposición, limitación y portabilidad de datos en los términos establecidos en el Reglamento (UE) 2016/679 de Protección de Datos mediante notificación escrita, aportando copia del DNI o tarjeta identificativa, a nuestro correo oposicionesarquitectosmadrid@gmail.com.
+
+// Usted puede consultar nuestra política de Protección de Datos en www.oposicionesarquitectos.com`
+    
+//       ;
+
+//       const mailOptions = {
+//         from: 'Oposiciones Arquitectos <academia.oposicionesarquitectos@gmail.com>',
+//         to: recipient,
+//         subject: subject,
+//         text: body,
+//         attachments: [
+//           {
+//             filename: invoice,
+//             path: path.join(selectedDirectory, invoice),
+//           },
+//         ],
+//       };
       
       // Si hay contenido adicional, agregarlo como adjunto (es el pdf opcional que agrega el usuario, temario...que se adjunta en algunos certificados)
-      if (pdfContent) {
-        mailOptions.attachments.push({
-          filename: 'temario.pdf',
-          content: Buffer.from(pdfContent.split(',')[1], 'base64'),
-        });
-      }
+    //   if (pdfContent) {
+    //     mailOptions.attachments.push({
+    //       filename: 'temario.pdf',
+    //       content: Buffer.from(pdfContent.split(',')[1], 'base64'),
+    //     });
+    //   }
       
 
-    try {
-      await transporter.sendMail(mailOptions);
-      console.log(`Correo para el alumno: ${student.ALUMNO} enviado a ${student.EMAIL}`);
+    // try {
+    //   await transporter.sendMail(mailOptions);
+    //   console.log(`Correo para el alumno: ${student.ALUMNO} enviado a ${student.EMAIL}`);
 
 
-      // Retraso de 15 segundos antes de enviar el siguiente correo
-      if (index < students.length - 1) {
-        await customDelay(15000);
-      }
-    } catch (error) {
-      console.error(`Error al enviar correo a ${student.ALUMNO}a la dirección ${student.EMAIL} no se ha enviado el certificado`, error);
-    }
+    //   // Retraso de 15 segundos antes de enviar el siguiente correo
+    //   if (index < students.length - 1) {
+    //     await customDelay(15000);
+    //   }
+    // } catch (error) {
+    //   console.error(`Error al enviar correo a ${student.ALUMNO}a la dirección ${student.EMAIL} no se ha enviado el certificado`, error);
+    // }
   
 
   const message = `Certificado guardado para el alumno: ${student.ALUMNO}`;
